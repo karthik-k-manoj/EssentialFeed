6425c833fe3e88222f4bc8b7f3f852e68028bc88
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-07-18 11:00:08 +0530

    move feed item to it's own file

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -7,19 +7,6 @@
 
 import Foundation
 
-public struct FeedItem: Equatable {
-    public let id: UUID
-    public let description: String?
-    public let location: String?
-    public let imageURL: URL
-    
-    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
-        self.id = id
-        self.description = description
-        self.location = location
-        self.imageURL = imageURL
-    }
-}
 
 public enum HTTPClientResult {
     case success(Data, HTTPURLResponse)
