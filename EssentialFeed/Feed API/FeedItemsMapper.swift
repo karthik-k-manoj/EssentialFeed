f9334744cb79e4d2d44f9f364af1ed338079fbb4
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-07-24 09:08:39 +0530

    move FeedItemsMapper to its own file

---
+++ EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -0,0 +1,38 @@
+//
+//  FeedItemsMapper.swift
+//  EssentialFeed
+//
+//  Created by Karthik K Manoj on 24/07/24.
+//
+
+import Foundation
+
+// Just a namspace for static methods. It does two kind of mapping. One is JSON data to `Root`
+// `RemoteFeedItem` to `FeedItem`
+internal final class FeedItemsMapper {
+    private struct Root: Decodable {
+        let items: [Item]
+    }
+
+    private struct Item: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
+        
+        var item: FeedItem {
+            FeedItem(id: id, description: description, location: location, imageURL: image)
+        }
+    }
+    
+    private static var OK_200: Int { 200 }
+
+    internal static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == OK_200 else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+        
+        let root = try JSONDecoder().decode(Root.self, from: data)
+        return root.items.map { $0.item }
+    }
+}
