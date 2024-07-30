153f2d716b2b1a34ddab180a64d460fcb0ee28dd
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-07-30 15:02:20 +0530

    resumes data task on creation

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -14,7 +14,7 @@ class URLSessionHTTPClient {
     }
     
     func get(from url: URL) {
-        session.dataTask(with: url, completionHandler: { _, _, _ in })
+        session.dataTask(with: url, completionHandler: { _, _, _ in }).resume()
     }
 }
 
@@ -22,24 +22,54 @@ final class URLSessionHTTPClientTests: XCTestCase {
     func test_getFromURL_createsDataTaskWithURL() {
         let url = URL(string: "http://any-url.com")!
         let session = URLSessionSpy()
-        let sut  = URLSessionHTTPClient(session: session)
+        let sut  = URLSessionHTTPClient  (session: session)
         
         sut.get(from: url)
         
         XCTAssertEqual(session.receivedURLs, [url])
     }
     
+    func test_getFromURL_resumesDataTaskWithURL() {
+        let url = URL(string: "http://any-url.com")!
+        let session = URLSessionSpy()
+        let task = URLSessionDataTaskSpy()
+        session.stub(url: url, task: task)
+        
+        let sut  = URLSessionHTTPClient  (session: session)
+        
+        sut.get(from: url)
+        
+        XCTAssertEqual(task.resumeCallCount, 1)
+         
+    }
+    
     private class URLSessionSpy: URLSession {
         var receivedURLs = [URL]()
+        private var stubs = [URL: URLSessionDataTask]()
+        
+        func stub(url: URL, task: URLSessionDataTask) {
+            stubs[url] = task
+        }
         
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
             receivedURLs.append(url)
-            return FakeURLSessionDataTask()
+            return stubs[url] ?? FakeURLSessionDataTask()
         }
     }
     
     // subclassing. often dangerous, we don't own their class. If we start mocking class we don't
-    // we are making good assumptions. Working solutions so we commit 
-    private class FakeURLSessionDataTask: URLSessionDataTask {}
+    // we are making good assumptions. Working solutions so we commit
+    // problem with those tests mocking class is test coupled with private implementation detail
+    // everytime we want to refactor the code then test break. tests are checking the exact impl. But we need to check the behaviour of the production code. Since test broke because we didn't call resume. 
+    private class FakeURLSessionDataTask: URLSessionDataTask {
+        override func resume() { }
+    }
+    private class URLSessionDataTaskSpy: URLSessionDataTask {
+        var resumeCallCount = 0
+        
+        override func resume() {
+            resumeCallCount += 1
+        }
+    }
 }
 
