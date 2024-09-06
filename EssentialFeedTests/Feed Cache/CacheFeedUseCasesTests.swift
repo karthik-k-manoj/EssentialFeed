73e7d80a961c88fa31efd585518bc113706324c8
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-09-06 10:58:43 +0530

    add `SaveResult` to protect code from potential breaking changes

--- EssentialFeedTests/Feed Cache/CacheFeedUseCasesTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCasesTests.swift
@@ -105,7 +105,7 @@ final class CacheFeedUseCasesTests: XCTestCase {
         let store = FeedStoreSpy()
         var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
         
-        var receivedResults = [Error?]()
+        var receivedResults = [LocalFeedLoader.SaveResult]()
         sut?.save([uniqueItem()]) { receivedResults.append($0) }
                 
         store.completeDeletionSuccessfully()
@@ -119,7 +119,7 @@ final class CacheFeedUseCasesTests: XCTestCase {
         let store = FeedStoreSpy()
         var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
         
-        var receivedResults = [Error?]()
+        var receivedResults = [LocalFeedLoader.SaveResult]()
         sut?.save([uniqueItem()]) { receivedResults.append($0) }
         
         sut = nil
